e3e32f5fd33b18c3df4dbd79b6afcee3c8d33d0e
Zhenrong Shi <shizhenrong1987@hotmail.com>
2020-08-23 15:12:36 -0400

    create an EC2 instance

--- 03/demos/demos/before/scripts/module_02/create-ec2-instance.js
+++ 03/demos/demos/before/scripts/module_02/create-ec2-instance.js
@@ -84,5 +84,22 @@ function createKeyPair (keyName) {
 }
 
 function createInstance (sgName, keyName) {
-  // TODO: create ec2 instance
+  const params = {
+    ImageId: 'ami-imageid under ec2 launch instance',
+    InstanceType: 't2.micro',
+    KeyName: keyName,
+    MaxCount: 1,
+    MinCount: 1,
+    SecurityGroups: [
+      sgName
+    ],
+    UserData: 'IyEvYmluL2Jhc2gNCmN1cmwgLS1zaWxlbnQgLS1sb2NhdGlvbiBodHRwczovL3JwbS5ub2Rlc291cmNlLmNvbS9zZXR1cF8xMi54IHwgc3VkbyBiYXNoIC0NCnN1ZG8geXVtIGluc3RhbGwgLXkgbm9kZWpzDQpzdWRvIHl1bSBpbnN0YWxsIC15IGdpdA0KZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9yeWFubXVyYWthbWkvaGJmbC5naXQNCmNkIGhiZmwNCm5wbSBpDQpucG0gcnVuIHN0YXJ0'
+  }
+
+  return new Promise((resolve, reject) => {
+    ec2.runInstances(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
 }
