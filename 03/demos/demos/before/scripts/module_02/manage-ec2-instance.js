b54a8358f17e2993b288fdf21a1b94cdcb6ac2eb
Zhenrong Shi <shizhenrong1987@hotmail.com>
2020-08-23 15:12:50 -0400

    managing EC2 instance

--- 03/demos/demos/before/scripts/module_02/manage-ec2-instance.js
+++ 03/demos/demos/before/scripts/module_02/manage-ec2-instance.js
@@ -1,20 +1,48 @@
 // Imports
 // TODO: Import the aws-sdk
+const AWS = require('aws-sdk')
 
 // TODO: Configure region
+AWS.config.update({ region: 'us-east-1'})
 
 // Declare local variables
 // TODO: Create an ec2 object
+const ec2 = new AWS.EC2()
 
 function listInstances () {
   // TODO: List instances using ec2.describeInstances()
+  return new Promise((resolve, reject) => {
+    ec2.describeInstances({}, (err, data) => {
+      if (err) reject(err)
+      // else resolve(data)
+      else {
+        resolve(data.Reservations.reduce((i, r) => {
+          return i.concat(r.Instances)
+        }, []))
+      }
+    })
+  })
 }
 
 function terminateInstance (instanceId) {
   // TODO: Terminate an instance with a given instanceId
+  const params = {
+    InstanceIds: [
+      instanceId
+    ]
+  }
+
+  return new Promise((resolve, reject) => {
+    ec2.terminateInstances(params, (err, data) => {
+      if(err) reject(err)
+      else resolve(data)
+    })
+  })
 }
 
 listInstances()
 .then(data => console.log(data))
-// terminateInstance()
+
+// instance id from listInstances
+// terminateInstance('i-0edcef834188a075a')
 // .then(data => console.log(data))
