5dfbf00cd0c2848472caa9b95887fdca04c32f12
Zhenrong Shi <shizhenrong1987@hotmail.com>
2020-08-23 16:44:02 -0400

    create a launch configuration

--- 03/demos/demos/after/scripts/module_03/create-launch-configuration.js
+++ 03/demos/demos/after/scripts/module_03/create-launch-configuration.js
@@ -1,10 +1,11 @@
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-AWS.config.update({ region: '/* TODO: add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
 // TODO: Create an autoscaling object
+const autoScaling = new AWS.AutoScaling()
 
 const lcName = 'hamsterLC'
 const roleName = 'hamsterLCRole'
@@ -17,4 +18,22 @@ helpers.createIamRole(roleName)
 
 function createLaunchConfiguration (lcName, profileArn) {
   // TODO: Create a launch configuration
+  const params = {
+    IamInstanceProfile: profileArn,
+    ImageId: 'ami-c4f83abe',
+    InstanceType: 't2.micro',
+    LaunchConfigurationName: lcName,
+    KeyName: keyName,
+    SecurityGroups: [
+      sgName
+    ],
+    UserData: 'IyEvYmluL2Jhc2gNCnN1ZG8gYXB0LWdldCB1cGRhdGUNCnN1ZG8gYXB0LWdldCAteSBpbnN0YWxsIGdpdA0KZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9yeWFubXVyYWthbWkvaGJmbC5naXQgL2hvbWUvYml0bmFtaS9oYmZsDQpjaG93biAtUiBiaXRuYW1pOiAvaG9tZS9iaXRuYW1pL2hiZmwNCmNkIC9ob21lL2JpdG5hbWkvaGJmbA0Kc3VkbyBucG0gaQ0Kc3VkbyBucG0gcnVuIHN0YXJ0'
+  }
+
+  return new Promise((resolve, reject) => {
+    autoScaling.createLaunchConfiguration(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
 }
