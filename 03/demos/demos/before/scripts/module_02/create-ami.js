4365377472e5b48ee23c71822da769eda2ad5487
Zhenrong Shi <shizhenrong1987@hotmail.com>
2020-08-23 15:29:42 -0400

    create an Amazon Machine Image

--- 03/demos/demos/before/scripts/module_02/create-ami.js
+++ 03/demos/demos/before/scripts/module_02/create-ami.js
@@ -1,14 +1,28 @@
 // Imports
 // TODO: Import the aws-sdk
+const AWS = require('aws-sdk')
 
 // TODO: Configure region
+AWS.config.update({ region: 'us-east-1'})
 
 // Declare local variables
 // TODO: Create an ec2 object
+const ec2 = new AWS.EC2()
 
-createImage('<instanceid>', 'hamsterImage')
+createImage('i-0edcef834188a075a', 'hamsterImage')
 .then(() => console.log('Complete'))
 
 function createImage (seedInstanceId, imageName) {
   // TODO: Implement AMI creation
+  const params = {
+    InstanceId: seedInstanceId,
+    Name: imageName
+  }
+
+  return new Promise((resolve, reject) => {
+    ec2.createImage(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
 }
