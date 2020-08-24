b07a82e608890789c97087ab28b0a5e26b835307
Zhenrong Shi <shizhenrong1987@hotmail.com>
2020-08-23 21:42:16 -0400

    create api and resource in api gateway

--- 03/demos/demos/after/scripts/module_06/create-api-gateway.js
+++ 03/demos/demos/after/scripts/module_06/create-api-gateway.js
@@ -1,10 +1,12 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+// AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
 // TODO: Create api gateway object
+const apiG = new AWS.APIGateway()
 const apiName = 'hamster-api'
 
 let apiData
@@ -24,25 +26,51 @@ createRestApi(apiName)
 
 function createRestApi (apiName) {
   // TODO: Create params const
+  const params = {
+    name: apiName
+  }
 
   return new Promise((resolve, reject) => {
     // TODO: Create a new rest API
+    apiG.createRestApi(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
 
 function getRootResource (api) {
   // TODO: Create params const
+  const params = {
+    restApiId: api.id
+  }
 
   return new Promise((resolve, reject) => {
     // TODO: Get the resources and find the resource with path '/'
+    apiG.getResources(params, (err, data) => {
+      if (err) reject(err)
+      else {
+        const rootResource = data.items.find(r => r.path === '/')
+        resolve(rootResource.id)
+      }
+    })
   })
 }
 
 function createResource (parentResourceId, resourcePath, api) {
   // TODO: Create params const
+  const params = {
+    parentId: parentResourceId,
+    pathPart: resourcePath,
+    restApiId: api.id
+  }
 
   return new Promise((resolve, reject) => {
     // TODO: Create the resource and return the resource id
+    apiG.createResource(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data.id)
+    })
   })
 }
 
