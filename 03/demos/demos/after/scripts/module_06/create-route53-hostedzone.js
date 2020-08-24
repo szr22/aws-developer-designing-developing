17a03b42512a2f4b5f0e74ba0fa16034a02003e8
Zhenrong Shi <shizhenrong1987@hotmail.com>
2020-08-23 21:32:40 -0400

    create hosted zone in route53

--- 03/demos/demos/after/scripts/module_06/create-route53-hostedzone.js
+++ 03/demos/demos/after/scripts/module_06/create-route53-hostedzone.js
@@ -1,10 +1,11 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
 // TODO: Create route53 object
+const route53 = new AWS.Route53()
 const hzName = 'hbfl.online'
 
 createHostedZone(hzName)
@@ -12,8 +13,16 @@ createHostedZone(hzName)
 
 function createHostedZone (hzName) {
   // TODO: Create params const
+  const params = {
+    CallerReference: `${Date.now()}`,
+    Name: hzName
+  }
 
   return new Promise((resolve, reject) => {
     // TODO: Create hostedzone with route53
+    route53.createHostedZone(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
