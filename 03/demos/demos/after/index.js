cd3518d19f61403732dce24a7c9a52f3c82114ea
Zhenrong Shi <shizhenrong1987@hotmail.com>
2020-08-23 21:05:24 -0400

    create ElasticCache Cluster

--- 03/demos/demos/after/index.js
+++ 03/demos/demos/after/index.js
@@ -6,17 +6,18 @@ const { init: queueInit } = require('./lib/data/lib/sqs.listener')
 
 const options = {
   port: process.env.PORT || 3000,
-  // // Commented out until Elasticache is configured
-  // cache: [{
-  //   name: 'redis',
-  //   provider: {
-  //     constructor: require('@hapi/catbox-redis'),
-  //     options: {
-  //       partition: 'cache',
-  //       host: 'your elasticache domain here',
-  //     }
-  //   }
-  // }]
+  // Commented out until Elasticache is configured
+  cache: [{
+    name: 'redis',
+    provider: {
+      constructor: require('@hapi/catbox-redis'),
+      options: {
+        partition: 'cache',
+        // host: 'your elasticache domain here',
+        host: 'hamster.792iyi.0001.use1.cache.amazonaws.com',
+      }
+    }
+  }]
 }
 
 const init = async () => {
@@ -26,7 +27,7 @@ const init = async () => {
 
   // hapi-auth-cookie stuff
   const cache = server.cache({
-    // cache: 'redis',
+    cache: 'redis',
     segment: 'sessions',
     expiresIn: 3 * 24 * 60 * 60 * 1000
   })
--- 03/demos/demos/after/scripts/module_05/create-redis-cluster.js
+++ 03/demos/demos/after/scripts/module_05/create-redis-cluster.js
@@ -5,6 +5,7 @@ const helpers = require('./helpers')
 AWS.config.update({ region: '/* TODO: Add your region */' })
 
 // TODO: Create an elasticache object
+const ec = new AWS.ElastiCache()
 
 helpers.createSecurityGroup('hamster_redis_sg', 6379)
 .then(sgId => createRedisCluster('hamster', sgId))
@@ -12,8 +13,21 @@ helpers.createSecurityGroup('hamster_redis_sg', 6379)
 
 function createRedisCluster (clusterName, sgId) {
   // TODO: Create params object
+  const params = {
+    CacheClusterId: clusterName,
+    CacheNodeType: 'cache.t2.micro',
+    Engine: 'redis',
+    NumCacheNodes: 1,
+    SecurityGroupIds: [
+      sgId
+    ]
+  }
 
   return new Promise((resolve, reject) => {
     // TODO: Create cache cluster
+    ec.createCacheCluster(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
